3b4f5bc17d5beb5ed614f0f610a8d8bf40b8f4be
zainulfakhri16 <zainul.fakhri2002@gmail.com>
2022-10-13 22:05:47 +0800

    update

---
+++ app/src/main/java/com/example/sehatco/ActivityKategori.kt
@@ -0,0 +1,12 @@
+package com.example.sehatco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityKategori : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kategori)
+
+    }
+}
